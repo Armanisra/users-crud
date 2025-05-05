439c13ccd7ea56d1dec5b9a3be5d437f319bb19a
Armanisra <israyelyannn@gmail.com>
2025-05-05 14:49:26 +0400

    initial commit

---
+++ users-models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Column, Integer, String 
+
+class Users():
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable = False) 
+    last_name = Column(String, nullable = False) 
+    age = Column(Integer, nullable = False)
